8a659a24065badfd25353fe27f46170b67a2e3c0
SerhiiMyronets <hardmail88@gmail.com>
2023-07-27 23:24:50 -0700

    vercel

--- src/midlewares/input-validation-middleware.ts
+++ src/midlewares/input-validation-middleware.ts
@@ -43,6 +43,6 @@ export const postBodyValidation = [
     body("shortDescription").trim().isLength({max: 100}).withMessage('ShortDescription length should be below 100 symbols'),
     body("content").trim().notEmpty().withMessage('Content is required'),
     body("content").trim().isLength({max: 1000}).withMessage('Content should be be below 1000 symbols'),
-    body("blogId").trim().notEmpty().withMessage('BlogId is required'),
-    body("blogId").trim().isString().withMessage('BlogId should be string'),
+    body("blogId")/*.trim()*/.notEmpty().withMessage('BlogId is required'),
+    body("blogId")/*.trim()*/.isString().withMessage('BlogId should be string'),
 ]
