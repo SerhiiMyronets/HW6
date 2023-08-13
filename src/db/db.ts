15705b7d45aa09484cae1d1f0f237d14ebcc8147
SerhiiMyronets <hardmail88@gmail.com>
2023-08-12 23:02:29 -0700

    add hw4

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,7 +3,7 @@ import {BlogInputMongoDB} from "../models/blogs-models";
 import {PostInputMongoDB} from "../models/posts-models";
 import 'dotenv/config'
 
-const mongoURI = 'mongodb://localhost:27017/'//process.env.MONGO_URI || "mongodb+srv://hardmail88:3846MrN1@cluster0.0goknaf.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = /*'mongodb://localhost:27017/'*/process.env.MONGO_URI || "mongodb+srv://hardmail88:3846MrN1@cluster0.0goknaf.mongodb.net/?retryWrites=true&w=majority"
 if (!mongoURI) {
     throw new Error('URI doesnt found!!')
 }
