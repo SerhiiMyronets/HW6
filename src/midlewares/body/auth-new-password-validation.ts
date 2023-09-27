e541c162db5ba081f6cbb61c8ddb1ba98963b9d9
SerhiiMyronets <hardmail88@gmail.com>
2023-09-26 20:44:45 -0700

    chahe

--- src/midlewares/body/auth-new-password-validation.ts
+++ src/midlewares/body/auth-new-password-validation.ts
@@ -2,5 +2,5 @@ import {body} from "express-validator";
 
 export const authNewPasswordValidation = [
     body('newPassword').exists().isString().isLength({min: 6, max: 20}).withMessage('Password length should be from 6 to 20 symbols.'),
-    body('recoveryCode').exists().isUUID().withMessage('Confirmation code is not valid'),
+    //body('recoveryCode').exists().isUUID().withMessage('Confirmation code is not valid'),
 ]
